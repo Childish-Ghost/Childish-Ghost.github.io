8f429dfc22818f34e36a244a8e0bdbe9e8f6eb29
Edward Cai <sntc06@gmail.com>
2014-05-22 16:30:00 +0800

    modified profile for sntc06

--- js/member.js
+++ js/member.js
@@ -165,7 +165,7 @@
         nick: '雪人',
         title: '融化中請稍候',
         identity: '',
-        avatar: 'http://farm3.staticflickr.com/2875/9069037713_1752f5daeb.jpg',
+        avatar: 'http://i.imgur.com/qKE4du5.png',
         content: '神秘的老機團副團長，曾多次協助電資島上的事務。對於電腦硬體有相當強的執著（?）。技能：雪人的祝福，（附加效果 智力-5 魅力-5 大叔+10）。'
       }, {
         id: 'WeiYan',
